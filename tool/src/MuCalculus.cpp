540da805dd75686197d4ac6ae2a93a3c748fc707
Valo13 <valo13@live.nl>
2016-02-27 16:42:53 +0100

    Added data structure for mu-calculus formula

---
+++ tool/src/MuCalculus.cpp
@@ -0,0 +1,41 @@
+#include "MuCalculus.h"
+#include "LinearTransitionSystem.h"
+
+#include <fstream>
+#include <string>
+
+
+
+//solves a mu calculus formula
+void MuFormula::solve(LinearTransitionSystem){
+	switch (op){
+	case FALSE:
+		//return empty set/list of states
+		break;
+	case TRUE:
+		//return full set/list of states
+		break;
+	case VAR:
+		//fixed point magic
+		break;
+	case AND:
+		//intersect set/list of states of the two subformula results
+		break;
+	case OR:
+		//unite set/list of states of the two subformula results
+		break;
+	case DIAMOND:
+		//diamond magic
+		break;
+	case BOX:
+		//box magic
+		break;
+	case MU:
+		//fixed point magic
+		break;
+	case NU:
+		//fixed point magic
+		break;
+
+	}
+}
--- tool/src/MuCalculus.h
+++ tool/src/MuCalculus.h
@@ -17,15 +17,57 @@
 #ifndef TOOL_MUCALCULUS_H_
 #define TOOL_MUCALCULUS_H_
 
-/**
- * This class contains methods to parse the mu-calculus file.
- */
-class MuCalculus
-{
-public:
+#include "LinearTransitionSystem.h"
 
-    //static ParseTree parseFile(const char* strFilename);
-    
+#include <string>
+
+/*
+Represents the possible operations or values of the mu-calculus
+*/
+enum Op {
+	FALSE,		//the value false
+	TRUE,		//the value true
+	VAR,		//a variable
+	AND,		//logical and
+	OR,			//logical or
+	DIAMOND,	//the diamond operator
+	BOX,		//the box operator
+	MU,			//the least fixed point operator
+	NU			//the greatest fixed point operator
+};
+
+/*
+Represents a mu-calculus formula, recursively
+
+subformula is only defined in case op in {AND, OR, DIAMOND, BOX, MU, NU}
+
+subformula2 is only defined in case op in {AND, OR}
+
+varlabel is only defined in case op in
+- {DIAMOND, BOX} then it is a transition label
+- {VAR, MU, NU} then it is a variable name
+
+prevFixedPoint is either
+- 'm' if the previous fixed point was a least fixed point
+- 'n' if the previous fixed point was a greatest fixed point
+- 'x' if there is no previous fixed point
+this is needed for the Emerson-Lei expansion on the algorithm
+*/
+class MuFormula
+{
+private:
+	MuFormula* subformula;
+	MuFormula* subformala2;
+	Op op;
+	std::string varlabel;
+	char prevFixedPoint;
+
+public:
+	//solves this mu-calculus formula
+	//TODO: change void to data type used for collection of states
+	void solve(LinearTransitionSystem);
+
+	static MuFormula parseMuFormula(const char* strFilename);
 };
 
 #endif // TOOL_MUCALCULUS_H_
