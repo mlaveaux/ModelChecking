ae2a1cede01869b3a4a3c90ba5f8747784c82d17
Maurice Laveaux <mauricelaveaux@gmail.com>
2016-03-14 18:26:52 +0100

    Additional script fixes and combined latex report into a single file

--- runtests.sh
+++ runtests.sh
@@ -5,13 +5,13 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 fi
 
 if [ ! -f "$1" ]; then
-    echo "$1" " doesn't exist";
-elif [ ! -f "$2"]; then
-    echo "$2" " doesn't exit";
+    echo "$1" " doesn't exist"; exit -1;
+elif [ ! -d "$2" ]; then
+    echo "$2" " doesn't exit"; exit -1;
 fi
 
 for file in $2/boolean/* $2/combined/* $2/fixpoints_only/* $2/modal_operators/*; do
     if [ ${file: -4} == ".mcf" ]; then
-        $1 --algo=naive testcases/fixpoints_only/test.aut $file
+        $1 --algo=improved testcases/fixpoints_only/test.aut $file
     fi
 done
