73bab3593a0da9aae8af88662be91564bf4a136f
Valo13 <valo13@live.nl>
2016-03-21 15:10:08 +0100

    very spoopy

---
+++ assignment 2/tool/src/PGSolver.cpp
@@ -0,0 +1,41 @@
+#include "PGSolver.h"
+#include <map>
+
+struct ProgMeasures {
+	int size;
+	int* maxMeasures;
+};
+
+/**
+ * Gets the progress measures for a parity game
+ */
+struct ProgMeasures getProgressMeasures(ParityGame game){
+
+}
+
+/**
+ * Computes whether a measure is greater than another measure
+ * limit: upto how many elements to check
+ * strict: whether it is strictly greater than or at least
+ */
+bool lexicoGreaterThan(int* measure1, int* measure2, int limit, bool strict){
+
+}
+
+/**
+ * Computes Prog
+ */
+int* prog(std::map<int, int*> parProgMeasures, int v, int w){
+
+}
+
+/**
+ * Bro do you even lift
+ */
+std::map<int, int*> lift(std::map<int, int*> parProgMeasures, int v){
+
+}
+
+static std::vector<bool> solveParityGame(ParityGame game){
+
+}
---
+++ assignment 2/tool/src/PGSolver.h
@@ -0,0 +1,7 @@
+#include <vector>
+
+
+/**
+ * Solves a parity game
+ */
+static std::vector<bool> solveParityGame(ParityGame game);
