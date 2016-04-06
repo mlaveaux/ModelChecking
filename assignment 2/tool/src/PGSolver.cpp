4cb13d445be117a9e2dfda33edd3a854f2ee24fd
Valo13 <valo13@live.nl>
2016-04-06 13:07:34 +0200

    Added explanation to the methods in the report.

--- assignment 2/tool/src/PGSolver.cpp
+++ assignment 2/tool/src/PGSolver.cpp
@@ -120,7 +120,6 @@ static Measures prog(const ParityGame& game, Measures& maxMeasures, const std::v
         }
         if (!madeStrictlyGreater) {
             newMeasure = TOP;
-            // does m = rhoish(w) = TOP also mean that rhoish(w) has to be adjusted?
         }
     }
 
@@ -144,6 +143,7 @@ Measures lift(const ParityGame& game, Measures maxMeasures, const std::vector<Me
 
         if (game.isEven(vertex)) {
             if (vertex == outgoingVertex && game.getPriority(vertex) % 2 == 0) {
+				//if it is a self loop with an even priority return the minimal measure
                 return Measures(game.getMaximumPriority() + 1, 0);
             }
 
